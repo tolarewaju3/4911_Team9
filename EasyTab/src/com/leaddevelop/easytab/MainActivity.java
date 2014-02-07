2f1b0b0e12aa51dad538187b544cf247215b9372
TiOluwa Olarewaju <tolarewaju3@gmail.com>
2014-02-07 14:40:32 -0500

    Parse libraries and connection testing
    Added Parse libraries
    Created Test Object on Backend

--- EasyTab/src/com/leaddevelop/easytab/MainActivity.java
+++ EasyTab/src/com/leaddevelop/easytab/MainActivity.java
@@ -3,13 +3,21 @@ package com.leaddevelop.easytab;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Parse.initialize(this, "X2S2BQQcTvCtg1bFVtHViTyy4bKXCvWrOuahnMut", "oNXU5Lz9lfgEiFafCil80dUAXJJjIFW3EVhUN8BF");
 		setContentView(R.layout.activity_main);
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
 	}
 
 	@Override
