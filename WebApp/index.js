651e18974e5686f694eae6681722d6d95a351682
emorphis <emorphis3@gatech.edu>
2014-02-23 01:01:33 -0500

    added more pages
    added main menu and account settings pages and made small modifications
    to existing pages

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
+
 	Parse.initialize("X2S2BQQcTvCtg1bFVtHViTyy4bKXCvWrOuahnMut", "6m2FrVnFYbapf0mRID6nSdsDeAxOoNcA9On30fSV");	
+
+	if (Parse.User.current()) {
+		window.location.replace("/mainmenu.html");
+	}
+
 	$(".loginForm").submit(function(event) {
+		event.preventDefault();
+
 		var username = $(".emailField").val();
 		var password = $(".passwordField").val();
 		Parse.User.logIn(username, password, {
 			success: function(user) {
-				alert("Login Successful!"); //placeholder until menu page is written
+				window.location.replace("/mainmenu.html");
 			},
 			error: function(user, error) {
 				$(".invalidCredentials").show();
 			}
 		});
-		event.preventDefault();
 	});
 });
