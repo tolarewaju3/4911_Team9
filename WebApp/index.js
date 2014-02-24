038df907839180b2533a187fb5683e67de50db7d
Ksion <sion.keanna@gmail.com>
2014-02-24 14:05:42 -0500

    Revert "added more pages"
    This reverts commit 651e18974e5686f694eae6681722d6d95a351682.

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,23 +1,16 @@
 $(document).ready(function() {
-
 	Parse.initialize("X2S2BQQcTvCtg1bFVtHViTyy4bKXCvWrOuahnMut", "6m2FrVnFYbapf0mRID6nSdsDeAxOoNcA9On30fSV");	
-
-	if (Parse.User.current()) {
-		window.location.replace("/mainmenu.html");
-	}
-
 	$(".loginForm").submit(function(event) {
-		event.preventDefault();
-
 		var username = $(".emailField").val();
 		var password = $(".passwordField").val();
 		Parse.User.logIn(username, password, {
 			success: function(user) {
-				window.location.replace("/mainmenu.html");
+				alert("Login Successful!"); //placeholder until menu page is written
 			},
 			error: function(user, error) {
 				$(".invalidCredentials").show();
 			}
 		});
+		event.preventDefault();
 	});
 });
