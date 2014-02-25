4ff7ea69d4dc8b2a11567e86ffe4b1a0e023d873
emorphis <emorphis3@gatech.edu>
2014-02-25 00:05:57 -0500

    added regex
    added regex to validate email, password, and taxPct inputs on front end.

--- WebApp/js/common.js
+++ WebApp/js/common.js
@@ -1,21 +1,24 @@
 var unknownErrorText = "An error occured. Please try again later.";
-var invalidLocationText = "Invalid location.";
-var invalidEmailText = "Invalid email.";
-var invalidPasswordText = "Invalid password.";
-var validTaxPctText = "Invalid tax percentage.";
+var invalidLocationText = "Please enter a location name.";
+var invalidEmailText = "Please use a valid email address.";
+var invalidPasswordText = "Password must be between 6 and 20 characters and may only use letters, numbers, and underscores.";
+var validTaxPctText = "Tax percentage must be a decimal to at most two decimal places (e.g. 7.5).";
 
 function validLocation(location) {
 	return location != "";
 }
 
 function validEmail(email) {
-	return email != "";
+	var re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+	return re.test(email);
 }
 
 function validPassword(password) {
-	return password != "";
+	var re = /^\w{6,20}$/
+	return re.test(password);
 }
 
 function validTaxPct(taxPct) {
-	return taxPct != "";
+	var re = /^(([1-9](\d)*)|[0]?)(\.\d{1,2})?$/
+	return re.test(taxPct);
 }
