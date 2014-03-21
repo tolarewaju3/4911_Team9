a8a67dab4805910bfbf6db9a21872334ef8080c3
emorphis <emorphis3@gatech.edu>
2014-03-20 23:14:39 -0400

    finished paypal cloud code
    added code for updating whether orders/bills have been paid for

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,3 +1,108 @@
+Parse.Cloud.define("updateAllOrdersArePaid", function(request, response) {
+	var user = request.params.user;
+	var orderQuery = new Parse.Query("Order");
+	orderQuery.equalTo("user", user);
+	orderQuery.equalTo("paid", false);
+	orderQuery.find({
+		success: function(orders) {
+			var numUnpaidOrders = orders.length;
+			var numOrders = orders.length;
+			orders.forEach(function(order) {
+				Parse.Cloud.run("updateOrderIsPaid", {orderId: order.id}, {
+					success: function(isPaid) {
+						if (isPaid) {
+							numUnpaidOrders--;
+						}
+						numOrders--;
+						if (numUnpaidOrders == 0) {
+							response.success(true);
+						}
+						else if (numOrders == 0) {
+							response.success(false);
+						}
+					}
+				});
+			});
+		}
+	});
+});
+
+Parse.Cloud.define("updateOrderIsPaid", function(request, response) {
+	var orderId = request.params.orderId;
+	var orderQuery = new Parse.Query("Order");
+	orderQuery.get(orderId, {
+		success: function(order) {
+			var billQuery = new Parse.Query("Bill");
+			billQuery.equalTo("order", order);
+			billQuery.equalTo("paid", false);
+			billQuery.find({
+				success: function(bills) {
+					var numUnpaidBills = bills.length;
+					var numBills = bills.length;
+					bills.forEach(function(bill) {
+						Parse.Cloud.run("updateBillIsPaid", {billId: bill.id}, {
+							success: function(isPaid) {
+								if (isPaid) {
+									numUnpaidBills--;
+								}
+								numBills--;
+								if (numUnpaidBills == 0) {
+									order.set("paid", true);
+									order.save();
+									response.success(true);
+								}
+								else if (numBills == 0) {
+									response.success(false);
+								}
+							}
+						});
+					});
+				}
+			});
+		}
+	});
+});
+
+Parse.Cloud.define("updateBillIsPaid", function(request, response) {
+	var billId = request.params.billId;
+	var billQuery = new Parse.Query("Bill");
+	billQuery.get(billId, {
+		success: function(bill) {
+			if (bill.get("payKey") && !bill.get("paid")) {
+				Parse.Cloud.httpRequest({
+					url: "https://svcs.sandbox.paypal.com/AdaptivePayments/PaymentDetails",
+					method: "POST",
+					headers: {	"Content-Type": "application/json",
+								"X-PAYPAL-SECURITY-USERID": "emorphis3-facilitator_api1.gatech.edu",
+								"X-PAYPAL-SECURITY-PASSWORD": "1394570559",
+								"X-PAYPAL-SECURITY-SIGNATURE": "A8KhtBow2Ql3SA0p7vWMiEAVNA3LAu1KMDzmNTcRICrYXzPQnGBH57RR",
+								"X-PAYPAL-APPLICATION-ID": "APP-80W284485P519543T",
+								"X-PAYPAL-REQUEST-DATA-FORMAT": "JSON",
+								"X-PAYPAL-RESPONSE-DATA-FORMAT": "JSON"},
+					body: {
+						"payKey": bill.get("payKey"),
+						"requestEnvelope": {
+							"errorLanguage":"en_US"
+						}
+					},
+					success: function(json) {
+						var paid = json.data.status == "COMPLETED";
+						bill.set("paid", paid);
+						bill.save();
+						response.success(paid);
+					}
+				});
+			}
+			else if (!bill.get("payKey")) {
+				response.success(false);
+			}
+			else if(bill.get("paid")) {
+				response.success(true);
+			}
+		}
+	});
+});
+
 Parse.Cloud.define("sendPaypalLinks", function(request, response) {
 	var payTo = "twizzenator@gmail.com"//request.params.user.get("email");
 	var orderId = request.params.orderId;
@@ -5,14 +110,17 @@ Parse.Cloud.define("sendPaypalLinks", function(request, response) {
 	orderQuery.get(orderId, {
 		success: function(order) {
 			var billQuery = new Parse.Query("Bill");
-			billQuery.equalTo("order", order)
+			billQuery.equalTo("order", order);
 			billQuery.find({
 				success: function(bills) {
 					bills.forEach(function(bill) {
 						var totalPrice = String(parseFloat(bill.get("priceWithTax")) + parseFloat(bill.get("tip"))) + "0";
 						var phoneNumber = bill.get("phoneNumber");
 						Parse.Cloud.run("getPaypalLink", {price: totalPrice, payTo: payTo}, {
-							success: function(link) {
+							success: function(payKey) {
+								var link = "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_ap-payment&paykey=" + payKey;
+								bill.set("payKey", payKey);
+								bill.save();
 								Parse.Cloud.run("sendPaypalLink", {link: link, phoneNumber: phoneNumber});
 							}
 						});
@@ -55,8 +163,6 @@ Parse.Cloud.define("getPaypalLink", function(request, response) {
 					"email": request.params.payTo
 				}]
 			},
-			"returnUrl":"http://google.com",
-			"cancelUrl":"http://google.com",
 			"requestEnvelope":{
 				"errorLanguage":"en_US",
 				"detailLevel":"ReturnAll"
@@ -64,8 +170,7 @@ Parse.Cloud.define("getPaypalLink", function(request, response) {
 		},
 		success: function(json) {
 			var payKey = json.data.payKey;
-			var link = "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_ap-payment&paykey=" + payKey;
-			response.success(link);
+			response.success(payKey);
 		}
 	});
 });
